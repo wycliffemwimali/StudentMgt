f28e632b58987125956c9e071ca589e1df19640d
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-08-30 19:11:37 +0300

    Student mgt app

---
+++ src/main/java/com/example/studentmgt/StudentMgtApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMgtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMgtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentmgt/StudentMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMgtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
