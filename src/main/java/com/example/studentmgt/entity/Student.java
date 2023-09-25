495c459a80efc9a332b4eb9554519c460e923491
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-09-25 08:26:36 +0300

    student entity

---
+++ src/main/java/com/example/studentmgt/entity/Student.java
@@ -0,0 +1,57 @@
+package com.example.studentmgt.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String secondname;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(String firstname, String secondname, String email) {
+        this.firstname = firstname;
+        this.secondname = secondname;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSecondname() {
+        return secondname;
+    }
+
+    public void setSecondname(String secondname) {
+        this.secondname = secondname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
